992defd5bcee4d4f4579b5e19e7090c7cd33e32d
Chidi Tuke <c2kproductions@gmail.com>
2024-06-15 09:52:06 -0400

    add clerk/convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://meet-dogfish-88.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
